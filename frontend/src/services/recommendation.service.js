3014405175c7250e9e42be0bb0fc131524948d36
Lais Matie <lmatiehara@gmail.com>
2025-08-28 16:40:17 -0300

    first commit

---
+++ frontend/src/services/recommendation.service.js
@@ -0,0 +1,13 @@
+// getRecommendations.js
+const getRecommendations = (
+  // eslint-disable-next-line
+  formData = { selectedPreferences: [], selectedFeatures: [] },
+  products
+) => {
+  /**
+   * Crie aqui a lógica para retornar os produtos recomendados.
+  */
+};
+
+// eslint-disable-next-line
+export default { getRecommendations };
