a0f629e30482fdda236346423e6072068fc05812
Lais Matie <lmatiehara@gmail.com>
2025-08-29 22:14:21 -0300

    style: improve columns in mobile devices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,18 +10,17 @@ function App() {
   };
 
   return (
-    <div className="bg-gray-100 min-h-screen flex flex-col justify-center items-center">
-      <h1 className="text-3xl font-bold mb-8">Recomendador de Produtos RD Station</h1>
-      <div className="bg-white p-8 rounded-lg shadow-md w-full md:w-3/4 lg:w-1/2 grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div className="col-span-2 mb-4">
-          <p className="text-lg">
-            Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode encontrar uma variedade de produtos da RD Station, cada um projetado para atender às necessidades específicas do seu negócio. De CRM a Marketing, de Conversas a Inteligência Artificial, temos uma solução para ajudar você a alcançar seus objetivos. Use o formulário abaixo para selecionar suas preferências e funcionalidades desejadas e receba recomendações personalizadas de produtos que melhor atendam às suas necessidades.
-          </p>
-        </div>
-        <div>
+    <div className="bg-[#F1F3F5] min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold my-8">Recomendador de Produtos RD Station</h1>
+      <div className="bg-white p-8 rounded-lg shadow-md w-full md:w-3/4 lg:w-1/2 ">
+        <p className="text-md md:text-lg mb-6">
+          Bem-vindo ao Recomendador de Produtos RD Station. Aqui você pode encontrar uma variedade de produtos da RD Station, 
+          cada um projetado para atender às necessidades específicas do seu negócio. De CRM a Marketing, de Conversas a Inteligência Artificial, 
+          temos uma solução para ajudar você a alcançar seus objetivos. Use o formulário abaixo para selecionar suas preferências e funcionalidades 
+          desejadas e receba recomendações personalizadas de produtos que melhor atendam às suas necessidades.
+        </p>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-8'>
           <Form onUpdateRecommendations={handleRecommendations}/>
-        </div>
-        <div>
           <RecommendationList recommendations={recommendations} />
         </div>
       </div>
